9dad3a81e54f96509dd5fa61e2c0d272f8be66fe
wanderley rabelo <padawandr@gmail.com>
2021-05-28 17:49:44 -0300

    feat: add calc methods and tests

--- src/calc.js
+++ src/calc.js
@@ -1,6 +1,8 @@
-module.exports = {
-  sum () {},
-  sub () {},
-  mult () {},
-  div () {}
-}
+const sum = (numA, numB) => (numA + numB)
+const sub = (numA, numB) => (numA - numB)
+const mult = (numA, numB) => (numA * numB)
+const div = (numA, numB) => (
+  (numB === 0) ? 'Cannot divide by zero' : (numA / numB)
+)
+
+export { sum, sub, mult, div }
--- tests/calc.spec.js
+++ tests/calc.spec.js
@@ -1,31 +1,56 @@
-const expect = require('chai').expect
-const calc = require('../src/calc')
+import { expect } from 'chai'
+import { sum, sub, mult, div } from '../src/calc'
 
 describe('Calc', () => {
 
   // smoke tests
   describe('Smoke tests', () => {
 
-    it('should exist the calc lib', () => {
-      expect(calc).to.exist
-    })
-
     it('should exist the method `sum`', () => {
-      expect(calc.sum).to.exist
+      expect(sum).to.exist
     })
 
     it('should exist the method `sub`', () => {
-      expect(calc.sub).to.exist
+      expect(sub).to.exist
     })
 
     it('should exist the method `mult`', () => {
-      expect(calc.mult).to.exist
+      expect(mult).to.exist
     })
 
     it('should exist the method `div`', () => {
-      expect(calc.div).to.exist
+      expect(div).to.exist
     })
 
+  })
+
+  // methods tests
+
+  describe('Sum', () => {
+    it('should return 5 when `sum(2,3)`', () => {
+      expect(sum(2,3)).to.be.equal(5)
+    })
+  })
+
+  describe('Sub', () => {
+    it('should return -1 when `sub(2,3)`', () => {
+      expect(sub(2,3)).to.be.equal(-1)
+    })
+  })
+
+  describe('Mult', () => {
+    it('should return 6 when `mult(2,3)`', () => {
+      expect(mult(2,3)).to.be.equal(6)
+    })
+  })
+
+  describe('Div', () => {
+    it('should return 1.5 when `div(3,2)`', () => {
+      expect(div(3,2)).to.be.equal(1.5)
+    })
+    it('should return "Cannot divide by zero" when divide by 0', () => {
+      expect(div(3,0)).to.be.equal('Cannot divide by zero')
+    })
   })
 
 })
